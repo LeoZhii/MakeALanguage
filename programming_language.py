2cec63b7f94f0dcf1bad805213f95d17802a393a
Vixlump <17090765+Vixlump@users.noreply.github.com>
2023-03-05 22:48:46 -0800

    added some comments

--- programming_language.py
+++ programming_language.py
@@ -1,11 +1,14 @@
 import sys
 
+#Store Script
 codex = []
 
 def build_codex():
 	global codex
+	#open script from command line argument
 	script = open(sys.argv[1], "r")
 	for read_file in script:
+		#read every line of file and place in codex
 		input_to_file = read_file.strip()
 		codex.append(input_to_file)
 	script.close()
@@ -15,7 +18,9 @@ def read_codex():
 	global codex
 	length = len(codex)
 	for line in range(length):
+		#read every line of codex and interpret instructions
 		if (codex[line]=="print"):
+			#add one to line and print next line after
 			line+=1
 			print(codex[line])
 read_codex()
